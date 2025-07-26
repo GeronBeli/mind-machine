de20bf29e91cbd071b16432d75add5a965f66f0b
GeronBeli <geronbeli@gmail.com>
2025-07-26 23:51:26 +0200

    refactor: Add file system and document vector handling classes for PDF processing inside utils

---
+++ backend/utils/neural_search/doc_vec.py
@@ -0,0 +1,35 @@
+"""
+DocVec Class
+
+Author: Abdelrahman Elsharkawi
+Creation Date: 11.11.2023
+"""
+import os
+
+class DocVec:
+    """
+    Class representing a document vector.
+
+    Attributes:
+    - path (str): Path to the document.
+    - name (str): Name of the document (extracted from the path).
+    - vec: Document vector.
+    - paras_vecs: List of dictionaries containing paragraph vectors and corresponding text
+    [{"paragraph":"","vec":""}].
+    """
+    def __init__(self, path, text, paras_vecs):
+        """
+        Initialize the DocVec class.
+
+        Parameters:
+        - path (str): Path to the document.
+        - vec: Document vector.
+        - paras_vecs: List of dictionaries containing paragraph vectors and corresponding text
+        [{"paragraph":"","vec":""}].
+        Returns:
+        None
+        """
+        self.path = path
+        self.name = os.path.basename(path)
+        self.text = text
+        self.paras_vecs = paras_vecs
---
+++ backend/utils/neural_search/pdf_reader.py
@@ -0,0 +1,76 @@
+"""
+PDF Helper Functions
+
+Author: Abdelrahman Elsharkawi
+Creation Date: 11.11.2023
+"""
+import PyPDF2
+from .doc_vec import DocVec
+import logging
+
+def split_string_to_chunks(input_string, chunk_length, overlap):
+    """
+    Splits an input string into chunks of specified length with a given overlap.
+
+    Args:
+        input_string (str): The input string to be split into chunks.
+        chunk_length (int): The desired length of each chunk.
+        overlap (int): The number of words to overlap between consecutive chunks.
+
+    Returns:
+        list: A list of chunks, where each chunk is a string of words.
+
+    """
+    words = input_string.split()
+    chunks = []
+
+    while words:
+        chunk = ' '.join(words[:chunk_length])
+        chunks.append(chunk)
+        words = words[chunk_length - overlap:]
+
+    return chunks
+
+def pdf_to_text(path, chunk_length, overlap):
+    """
+    Convert PDF file to plain text.
+
+    Parameters:
+    - path (str): Path to the PDF file.
+
+    Returns:
+    dict: {'text':extracted_text, 'paragraphs':paragraphs}.
+    """
+    pdffileobj=open(path,'rb')
+    pdfreader=PyPDF2.PdfReader(pdffileobj)
+    extracted_text = ""
+    paragraphs = []
+
+    for page in pdfreader.pages:
+        extracted_text += page.extract_text()
+
+    paragraphs = split_string_to_chunks(extracted_text, chunk_length, overlap)
+    return {'text':extracted_text, 'paragraphs':paragraphs}
+
+def pdf_to_docVec(path, encoder, chunk_length = 0, remove_stop_words=False, overlap = 30):
+    """
+    Convert PDF file to DocVec object.
+
+    Parameters:
+    - path (str): Path to the PDF file.
+    - encoder: Sentence embeddings encoder.
+
+    Returns:
+    DocVec: DocVec object containing document vectors and paragraph vectors.
+    """
+    if chunk_length == 0:
+        chunk_length = encoder.max_seq_length
+    doc = pdf_to_text(path, chunk_length, overlap)
+    paras_vecs = []
+    for idp, para in enumerate(doc['paragraphs']):
+        paras_vecs.append({"paragraph":para,"vec":encoder.encode(para).tolist()})
+        #print(paras_vecs)
+        logging.debug(f"Paragraph {idp} vectorized: {para}")
+
+
+    return DocVec(path, doc['text'], paras_vecs)
